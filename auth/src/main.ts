2563849b12d0b51863b1918c12261b5dc134c1a1
mint_duc <mintduc2612@gmail.com>
2024-09-11 22:47:41 +0700

    auth_service

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './application/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 3001
+    }
+  });
+  await app.startAllMicroservices();
   await app.listen(3001);
   console.log('listen port 3001')
 }
