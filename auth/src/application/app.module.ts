9308a62d338aa0a9541dc74803300f4b9c80bcb4
Kevin Tran <kevin.tran@techvify.com.vn>
2024-09-12 15:01:01 +0700

    add-config

--- auth/src/application/app.module.ts
+++ auth/src/application/app.module.ts
@@ -1,27 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConFig } from 'src/configs/mysqlDB.config';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
-import { join } from 'path';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
-const TypeOrmConFig = TypeOrmModule.forRootAsync({
-  imports: [ConfigModule],
-  useFactory: (configService: ConfigService) => ({
-    type: 'mysql',
-    host: configService.get('DB_HOST'),
-    port: +configService.get('DB_PORT'),
-    username: configService.get('DB_USERNAME'),
-    password: configService.get('DB_PASSWORD'),
-    database: configService.get('DB_DATABASE'),
-    entities: [join(process.cwd(), 'dist/**/*.entity.js')],
-    synchronize: true,
-  }),
-  inject: [ConfigService],
-});
 
 @Module({
-  imports: [TypeOrmConFig],
+  imports: [TypeOrmModule.forRootAsync(TypeOrmConFig)],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- user-service/src/application/app.module.ts
+++ user-service/src/application/app.module.ts
@@ -1,30 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConFig } from 'configs/mysqlDB.config';
+import { Users } from 'src/entities/user.entity';
 import { UserController } from './controllers/user.controller';
 import { UserService } from './services/user.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { join } from 'path';
-import { Users } from 'src/entities/user.entity';
-
-const TypeOrmConFig = TypeOrmModule.forRootAsync({
-  imports: [ConfigModule],
-  useFactory: (configService: ConfigService) => ({
-    type: 'mysql',
-    host: configService.get('DB_HOST'),
-    port: +configService.get('DB_PORT'),
-    username: configService.get('DB_USERNAME'),
-    password: configService.get('DB_PASSWORD'),
-    database: configService.get('DB_DATABASE'),
-    entities: [join(process.cwd(), 'dist/**/*.entity.js')],
-    synchronize: true,
-  }),
-  inject: [ConfigService],
-});
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmConFig,
+    TypeOrmModule.forRootAsync(TypeOrmConFig),
     TypeOrmModule.forFeature([Users]),
   ],
   controllers: [UserController],
